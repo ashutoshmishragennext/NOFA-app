b2a3b300e5152a885e41ff4a59d593e6324afebc
AnuragOP01 <ranurag404@gmail.com>
2025-09-08 16:50:14 +0530

    removed iso duplicate env

--- components/utils/GoogleAuth.ts
+++ components/utils/GoogleAuth.ts
@@ -8,7 +8,7 @@ WebBrowser.maybeCompleteAuthSession();
 export const useGoogleAuth = () => {
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
-    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
+    // iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
     scopes: ['profile', 'email'],
     redirectUri: makeRedirectUri({
